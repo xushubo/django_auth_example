a9d62014ef32c1216f31e969a594beda627e6874
xushubo <tmac523@163.com>
2018-05-30 17:50:22 +0800

    init

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', context={'form': form})
+
+def index(request):
+    return render(request, 'index.html')
